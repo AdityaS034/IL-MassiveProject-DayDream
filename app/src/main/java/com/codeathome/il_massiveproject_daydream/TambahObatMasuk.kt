66bb0e25ef791eef04cec502ce16864ca0745402
Adi Nugraha Kartiwa <17200335@bsi.ac.id>
2023-11-22 14:29:43 +0700

    medistock

---
+++ app/src/main/java/com/codeathome/il_massiveproject_daydream/TambahObatMasuk.kt
@@ -0,0 +1,11 @@
+package com.codeathome.il_massiveproject_daydream
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TambahObatMasuk : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tambah_obat_masuk)
+    }
+}
